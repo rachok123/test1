d8a3eea9e5e4bf4e8c5cd1cc0747be7258a985db
rachok123 <rachok2012@gmail.com>
2015-11-05 16:30:37 +0200

    Refactor

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,19 +4,21 @@
     action :install
   end
 end
-bash 'gnutls downgrade' do
+execute 'gnutls downgrade' do
   code <<-EOH
   yum downgrade ftp://bo.mirror.garr.it/pub/1/slc/centos/7.0.1406/updates/x86_64/Packages/gnutls-3.1.18-10.el7_0.x86_64.rpm
   echo "exclude=gnutls*" >> /etc/yum.conf
   EOH
+  action :run
 end
 
 
 
-bash 'zabbix_repo_configure' do
+execute 'zabbix_repo_configure' do
   code <<-EOH
     rpm -i  http://repo.zabbix.com/zabbix/2.4/rhel/7/x86_64/zabbix-release-2.4-1.el7.noarch.rpm
   EOH
+  action :run
   only_if do
    !File.exists?('/etc/yum.repos.d/zabbix.repo')
   end
@@ -35,47 +37,31 @@ service 'mariadb' do
   action [:enable, :start]
 end
 
-
-
-
-
-bash 'mysql_secure_installation' do
-  code <<-EOH
-    mysql -uroot<<EOF  &&  touch /root/.mysql_secure_installation_complete
--- remove anonymous users
-DELETE FROM mysql.user WHERE User='';
--- create user zabbix
-CREATE USER 'zabbix'@'localhost' IDENTIFIED BY 'zabbix';
-GRANT ALL PRIVILEGES ON *.* TO 'zabbix'@'localhost';
--- Disallow root login remotely
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
--- Remove test database and access to it
-DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
--- Reload privilege tables now
-FLUSH PRIVILEGES;
--- Create Database
-CREATE DATABASE zabbix;
-EOF
-  EOH
-  only_if do
-    !File.exists?('/root/.mysql_secure_installation_complete')
-  end
+execute 'mysql_secure_installation' do
+  command 'mysql -uroot < /tmp/mysql_prepeare.sql && touch /root/.mysql_secure_instaled'
+  action :nothing
 end
 
-bash 'mysql_zabbix_installation' do
-  code <<-EOH
-    cd /usr/share/doc/zabbix-server-mysql-2.4.6//create
-    mysql -uroot zabbix < schema.sql
-    mysql -uroot zabbix < images.sql
-    mysql -uroot zabbix < data.sql
-    touch /root/.mysql_secure_installation_complete
-  EOH
+template '/tmp/mysql_prepeare.sql' do
+  source 'mysql-secure.sql.erb'
+  notifies :run, 'execute[mysql_secure_installation]', :immediately
   only_if do
     !File.exists?('/root/.mysql_mysql_installation_complete')
   end
 end
 
+execute 'zabbix-import' do
+  command '/tmp/zabix-import.sh'
+  action :nothing
+end
+
+template '/tmp/zabix-import.sh' do
+  source 'zabbix-import.sh.erb'
+  notifies :run, 'execute[zabbix-import]', :immediately
+  only_if do
+    !File.exists?(' /root/.mysql_import_complete')
+  end
+end
 
 template "/etc/zabbix/zabbix_server.conf" do
   source "zabbix_server.conf.erb"
@@ -92,8 +78,8 @@ cookbook_file "/etc/httpd/conf.d/zabbix.conf" do
 end
 
 
-"#{%w{mariadb zabbix-server httpd}.each do |zabbix_service|
-  service "#{zabbix_service}" do
-    action :restart
-  end
-end}"
+#{%w{mariadb zabbix-server httpd}.each do |zabbix_service|
+#  service "#{zabbix_service}" do
+#  action :restart
+#  end
+#end}"
