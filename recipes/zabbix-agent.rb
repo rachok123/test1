8ac9ff227d36bc05672d7ec3106bbe3b03700412
rachok123 <rachok2012@gmail.com>
2015-11-16 15:15:59 +0200

    Add Windows client

--- recipes/zabbix-agent.rb
+++ recipes/zabbix-agent.rb
@@ -22,6 +22,38 @@ case node["platform"]
     end
   when "windows"
 
+    windows_zipfile 'c:/zabbix' do
+      source 'http://www.zabbix.com/downloads/2.4.1/zabbix_agents_2.4.1.win.zip'
+      action :unzip
+      not_if {::File.exists?('c:/zabbix/bin/win64/zabbix_agentd.exe')}
+    end
 
+    template 'c:/zabbix/bin/win64/zabbix_agentd.exe' do
+      source  'zabbix-agent.conf.erb'
+      action :create_if_missing
+      inherits true
+    end
+
+    windows_package 'Zabbix agentd' do
+      source 'c:/zabbix/bin/win64/zabbix_agentd.exe'
+      options '--config c:/zabbix/conf/zabbix_agentd.win.conf  --install'
+      action :install
+    end
+
+    execute 'Zabbix add rules port in 10050' do
+      timeout 5
+      command "netsh advfirewall firewall add rule name='Zabbix agent port 10050' dir=in action=allow program='C:\Zabbix\bin\win64\zabbix_agentd.exe' protocol=TCP localport=10050 enable=yes"
+    end
+
+    execute 'Zabbix add rules port out 10050' do
+      timeout 5
+      command "netsh advfirewall firewall add rule name='Zabbix agent port 10050' dir=out action=allow program='C:\Zabbix\bin\win64\zabbix_agentd.exe' protocol=TCP localport=10050 enable=yes"
+    end
+
+    service "Zabbix Agent" do
+      supports :status => true, :start => true, :stop => true, :restart => true
+      action [ :enable ]
+      action [ :start ]
+    end
 
 end
